178e60785f17d58fc6a54ca632bcdbc1306d15d7
Michael Byers <mikebyers24@gmail.com>
2024-07-06 12:02:53 -0400

    working silent auction.

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+print('Welcome to the silent auction program.')
+
+still_biding = True
+
+def calculate_winner():
+  winner_name = ''
+  winner_bid = 0
+  for name, bid in dictionary.items():
+    if(bid > winner_bid):
+      winner_bid = bid
+      winner_name = name
+  print(f"The winner is {winner_name} with a bid of {winner_bid}")
+
+dictionary = {}
+
+while(still_biding):
+  
+  name = input('What is your name?: \n')
+  bid = float(input("What is your bid?: \n$"))
+  dictionary[name] = bid
+
+  other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+
+  if(other_bidders == 'no'):
+    calculate_winner()
+    still_biding = False
+  elif(other_bidders == 'yes'):
+    os.system('clear')
+
